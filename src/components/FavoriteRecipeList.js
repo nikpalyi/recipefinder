8eee61c920d7456c9465bbe3fb97c9e90c7aa40e
nikpalyi <nikolett.palyi@wipro.com>
2019-08-07 11:12:36 +0100

    listing favorites in diff list under normal

---
+++ src/components/FavoriteRecipeList.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+// import { Link } from 'react-router-dom';
+import RecipeItem from './RecipeItem';
+
+class FavoriteRecipeList extends Component {
+  render() {
+    return (
+      <div>
+        <h4>Favourite Recipes</h4>
+        {this.props.favoriteRecipes.map((recipe, index) => {
+          return <RecipeItem key={index} recipe={recipe} />;
+        })}
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    favoriteRecipes: state.favoriteRecipes
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  null
+)(FavoriteRecipeList);
